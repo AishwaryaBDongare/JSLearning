5a8c90f43bc9ef03ce01910e92704bd41608ca5f
AishwaryaBDongare <aishwaryabdongare0710@gmail.com>
2023-07-31 15:12:38 +0530

    for and while loop
    for and while loop

---
+++ 06_forLoop.js
@@ -0,0 +1,101 @@
+console.log(`------------step 1----------------`);
+
+
+
+for (let index = 5; index <=15; index++) {
+console.log(index);    
+}
+
+console.log(`------------step 2----------------`);
+for (let index =50; index >=40; index--) {
+console.log(index);    
+}
+
+console.log(`------------step 3----------------`);
+
+for (let index = 1; index <=30; index=index+2) {
+console.log(index);    
+}
+
+
+console.log(`------------step 4----------------`);
+for (let index = 0; index <=20; index=index+2) {
+console.log(index);    
+}
+
+console.log(`------------step 5----------------`);
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);
+    
+}
+
+console.log(`------------step 6----------------`);
+for (let index = 10; index <=100; index=index+10) {
+    console.log(index);
+    
+}
+
+console.log(`------------step 6----------------`);
+for (let index = 100; index >=10; index=index-10) {
+    console.log(index);
+    
+}
+
+
+console.log(`------------While loop----------------`);
+
+console.log(`------------step 1----------------`);
+
+var i =5;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+console.log(`------------step 2----------------`);
+
+var i=50;
+while (i>=40) {
+    console.log(i);
+    i--;
+}
+
+console.log(`------------step 3----------------`);
+
+var i=1;
+while (i<=30) {
+    console.log(i);
+    i=i+2;
+    
+}
+console.log(`------------step 4----------------`);
+
+var i=0;
+while (i<=20) {
+    console.log(i);
+    i=i+2;
+    
+}
+console.log(`------------step 5----------------`);
+
+
+var i=5;
+while (i<=50) {
+    console.log(i);
+    i=i+5;
+    
+}
+console.log(`------------step 6----------------`);
+
+var i=10;
+while (i<=100) {
+    console.log(i);
+    i=i+10;
+    
+}
+console.log(`------------step 7----------------`);
+var i=100;
+while (i>=10) {
+    console.log(i);
+    i=i-10;
+    
+}
